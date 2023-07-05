f1b63201d37f93e2912aacfbda6e51fa18a7b5bf
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2023-07-05 20:14:21 +0200

    Updated task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,17 +8,17 @@ import requests
 import sys
 
 
-def get_name():
+def get_name(employee_id):
     """
     Gets the user name based on the employee ID.
     """
-    payload = {'id': sys.argv[1]}
+    payload = {'id': employee_id}
     response = requests.get('https://jsonplaceholder.typicode.com/users', params=payload)
     user_data = response.json()
     return user_data[0]['name']
 
 
-def get_task():
+def get_task(employee_id):
     """
     Retrieves task numbers and completed todos.
     """
@@ -29,17 +29,27 @@ def get_task():
     data = response.json()
 
     for item in data:
-        if item['userId'] == int(sys.argv[1]):
+        if item['userId'] == employee_id:
             tasks_todo += 1
             if item['completed']:
                 todo_list.append(item['title'])
                 tasks_done += 1
 
-    print("Employee {} is done with tasks({}/{}):".format(get_name(), tasks_done, tasks_todo))
+    employee_name = get_name(employee_id)
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name,
+        tasks_done,
+        tasks_todo
+    ))
 
     for todo in todo_list:
         print("\t{}".format(todo))
 
 
 if __name__ == "__main__":
-    get_task()
+    if len(sys.argv) != 2:
+        print("Usage: {} employee_id".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_task(employee_id)
