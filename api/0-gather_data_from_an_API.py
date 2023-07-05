6b16c444ff5c89ede051b03a86841652ea23b175
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2023-07-05 20:10:21 +0200

    Updated

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,45 @@
 #!/usr/bin/python3
-"""A python script that uses ths REST API"""
+
+"""
+Module: gather_data_from_an_API
+"""
 
 import requests
 import sys
 
-"""Functions for gathering data from an API"""
+
+def get_name():
+    """
+    Gets the user name based on the employee ID.
+    """
+    payload = {'id': sys.argv[1]}
+    response = requests.get('https://jsonplaceholder.typicode.com/users', params=payload)
+    user_data = response.json()
+    return user_data[0]['name']
+
+
+def get_task():
+    """
+    Retrieves task numbers and completed todos.
+    """
+    response = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todo_list = []
+    tasks_todo = 0
+    tasks_done = 0
+    data = response.json()
+
+    for item in data:
+        if item['userId'] == int(sys.argv[1]):
+            tasks_todo += 1
+            if item['completed']:
+                todo_list.append(item['title'])
+                tasks_done += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(get_name(), tasks_done, tasks_todo))
+
+    for todo in todo_list:
+        print("\t{}".format(todo))
+
 
 if __name__ == "__main__":
-  employee_id = sys.argv[1]
-  url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-  todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
-    todo = todo.format(employee_id)
-
-    user_info = requests.request("GET", url).json()
-    todo_info = requests.request("GET", todo).json()
-
-    employee_name = user_info.get("name")
-    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
-    task_com = len(total_tasks)
-    total_task_done = len(todo_info)
-
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-          task_com, total_task_done))
-
-    [print("\t {}".format(task.get("title"))) for task in total_tasks]
+    get_task()
